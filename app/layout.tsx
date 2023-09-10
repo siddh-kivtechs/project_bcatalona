e2275ba8d31432af4cdaa96919d2f289a9616f8f
siddht1 <rt1yrru+github@gmail.com>
2023-09-10 19:14:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "CATALONA|CAT-3A",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "CATALONA|CAT-3A ",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
