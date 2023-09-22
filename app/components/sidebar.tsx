66ea4a190bf28943e63e2a4019c7a5e49c4fb2cc
siddht1 <rt1yrru+github@gmail.com>
2023-09-22 11:20:16 +0530

    Update sidebar.tsx
    modify name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,9 +90,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT by KIVTECHS</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+              Project CATALONA by KIVTECHS
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
